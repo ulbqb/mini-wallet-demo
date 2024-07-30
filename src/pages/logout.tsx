963e9d20a3102b22dd773c3a237884071275f975
Shogo Hyodo <mmoshg8u@gmail.com>
2024-07-30 16:56:17 +0900

    Add logout page (#6)
    * Add logout page
    
    * Fix

---
+++ src/pages/logout.tsx
@@ -0,0 +1,21 @@
+import PageHeader from '@/components/PageHeader'
+import type { Liff } from '@line/liff'
+import type { NextPage } from 'next'
+import { Fragment } from 'react'
+import { Web3AuthNoModal } from '@web3auth/no-modal'
+
+const Logout: NextPage<{ liff: Liff | null; web3auth: Web3AuthNoModal | null }> = ({
+  liff,
+  web3auth
+}) => {
+  web3auth?.logout().then(() => {
+    liff?.closeWindow()
+  })
+  return (
+    <Fragment>
+      <PageHeader title="Logout" />
+    </Fragment>
+  )
+}
+
+export default Logout
