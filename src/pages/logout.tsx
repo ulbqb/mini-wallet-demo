40453c04d8a6e6dd0e6cca9e66dfe84fc8ef39f0
Shogo Hyodo <mmoshg8u@gmail.com>
2024-07-30 17:52:06 +0900

    Use LIFF login on not LIFF browser (#7)
    * Use LIFF login in PC
    
    * Add liff logout

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -9,6 +9,7 @@ const Logout: NextPage<{ liff: Liff | null; web3auth: Web3AuthNoModal | null }> 
   web3auth
 }) => {
   web3auth?.logout().then(() => {
+    liff?.logout()
     liff?.closeWindow()
   })
   return (
